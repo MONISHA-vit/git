1276778b3a33f2bc9079e71af547c2d4e1d68050
MONISHA-vit <monishadevaraj238@gmail.com>
2026-02-05 18:08:18 +0530

    updated java file

---
+++ sub.java
@@ -0,0 +1,9 @@
+
+public class sub {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+        int result = a - b;
+        System.out.println("Subtraction = " + result);
+    }
+}
