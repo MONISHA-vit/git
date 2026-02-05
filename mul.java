2e6e6a45e99efde6a6d16db8310d960f36b84372
MONISHA-vit <monishadevaraj238@gmail.com>
2026-02-05 19:09:00 +0530

    java

---
+++ mul.java
@@ -0,0 +1,9 @@
+
+public class mul {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 4;
+        int result = a * b;
+        System.out.println("Multiplication = " + result);
+    }
+}
