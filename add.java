1247bcbcb3ef2d400f78f7667f7302730ed13626
MONISHA-vit <monishadevaraj238@gmail.com>
2026-02-05 18:04:36 +0530

    updated java file

---
+++ add.java
@@ -0,0 +1,9 @@
+public class add {
+    public static void main(String[] args) {
+        System.out.println("Simple Java Program");
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+}
